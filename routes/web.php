0d9d2e25bde540aff9e0bf336ae459fd6b3956da
NguyenCongDuy <146914945+NguyenCongDuy@users.noreply.github.com>
2025-04-07 20:22:36 +0700

    Chức năng quên mật khẩu

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/', function () {
+    return view('client');
+})->middleware(['auth', 'verified'])->name('client');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
